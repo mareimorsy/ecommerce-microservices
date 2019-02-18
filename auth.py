7033428c898b74b7e218fa75f656af7cea379583
Marei Morsy <MareiMorsy>
2019-02-18 23:28:32 +0200

    adding auth service

---
+++ auth.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+users = [
+    User(1, 'marei', '123'),
+    User(2, 'hamdi', '123'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['JWT_VERIFY_EXPIRATION'] = False
+
+
+jwt = JWT(app, authenticate, identity)
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
+
+# This is gonna be inside order service I'm just trying it here
+@app.route('/order')
+@jwt_required()
+def order():
+    return 'user id # %s has ordered some books ! :D' %  current_identity.id
+
+if __name__ == '__main__':
+    app.run()
